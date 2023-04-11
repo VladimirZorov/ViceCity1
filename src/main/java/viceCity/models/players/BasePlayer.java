e676793e757c805730fa7224a42ccb78c45ab254
Vladimir Zorov <vl.zorov@gmail.com>
2023-04-11 18:50:56 +0300

    add

--- src/main/java/viceCity/models/players/BasePlayer.java
+++ src/main/java/viceCity/models/players/BasePlayer.java
@@ -3,17 +3,23 @@ package viceCity.models.players;
 import viceCity.models.guns.Gun;
 import viceCity.repositories.interfaces.Repository;
 
+import java.util.ArrayList;
+
 public class BasePlayer implements Player{
 
     private String name;
     private int lifePoints;
     private Repository<Gun> gunRepository;
 
-
+    public BasePlayer(String name, int lifePoints) {
+        this.name = name;
+        this.lifePoints = lifePoints;
+        this.gunRepository =
+    }
 
     @Override
     public String getName() {
-        return null;
+        return this.name;
     }
 
     @Override
